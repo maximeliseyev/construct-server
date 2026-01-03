1d036a3f4f52b0039f1489428403424750984362
maximeliseyev <maxim7eli@gmail.com>
2026-01-03 15:01:15 +0300

    upgrade apn security

--- src/apns/mod.rs
+++ src/apns/mod.rs
@@ -1,5 +1,7 @@
 pub mod client;
+pub mod encryption;
 pub mod types;
 
 pub use client::ApnsClient;
+pub use encryption::DeviceTokenEncryption;
 pub use types::{ApnsPayload, NotificationPriority, PushType};
